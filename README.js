5a927527981d04e0f663755149b7c3748c456191
MartimSilva07 <78629666+MartimSilva07@users.noreply.github.com>
2021-02-06 03:11:13 +0000

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,26 @@
+📌 A little more about me...
+const MartimSilva07 = {
+    firstName: "Martim",
+    lastName: "Silva",
+    age: 15,
+    country: "Portugal",
+    frontend: "Javascript,HTML,CSS,React,vue,typescript"
+    languagesIWantedToLearn: ["Python", "Java"],
+    askMeAbout: ["Web development", "Mobile development", "Technology", "gaming development.],
+    phraseThatIdentifiesMe: "Coding My Future",
+    operatingSystems: {
+        windows: "Experient",
+        linux: "Basic",
+        macOS: "Intermediate"
+    },
+    technologies: {
+         frontend: "Javascript,HTML,CSS,React,vue,typescript"
+        },
+        backEnd: {
+            Node.js,Python, Java,PHP etc...
+ 
+        },
+        databases: ["Mongo", "MySQL", "SQLite"]
+    },
+    Mydream: "be the CEO or CEO of a large company"
+};
